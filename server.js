858594a42f7362082ad5e4a402f765d8c3e222d4
Rohan Prasad <studentrrp@gmail.com>
2020-10-23 09:55:32 +0530

    Initial Commit

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json())
 //Anything that has api/items in its url should go 
 //to the items file that we import from routes
 app.use('/api/items', items)
-app.use(express.static('public'))
+const port = process.env.PORT || 5000;
 
 const db = require('./config/keys').mongoURI;
 
@@ -22,11 +22,12 @@ try {
     mongoose.connect(db, {
         useNewUrlParser: true,
         useUnifiedTopology: true
-    }, () =>
-        console.log("connected"));
+    }, () => {
+        console.log("connected")
+        app.listen(port, () => console.log('Running'))
+    }
+    );
 } catch (error) {
     console.log("could not connect");
 }
-const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log('Running'))
